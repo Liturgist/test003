f7e7c3e497bad68a4e25749377c0b1858b3ffd13
Paul Watson <paul.hermeneutic@gmail.com>
2023-04-05 16:35:58 -0500

    Changed fill character.

--- New-TestFile.ps1
+++ New-TestFile.ps1
@@ -1,8 +1,8 @@
 $FilePrefix = 'PatTest'
 $Pattern = 'THE_PATTERN'
-$FillCharacter = 'x'
+$FillCharacter = 'y'
 
 for (($i = 1000); $i -lt 1500; $i++) {
     $FileName = "$FilePrefix-$i.txt"
-    Set-Content -Path $FileName -Value ("$($FillCharacter * $i))" + $Pattern + "$($FillCharacter * 10)")
+    Set-Content -Path $FileName -Value ("$($FillCharacter * $i)" + $Pattern + "$($FillCharacter * 10)")
 }
