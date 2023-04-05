f992e022c7c667f276480acd65ca450b1268d40d
Paul Watson <paul.hermeneutic@gmail.com>
2023-04-05 17:15:30 -0500

    Added test 'content' pattern as a comment.

--- New-TestFile.ps1
+++ New-TestFile.ps1
@@ -6,3 +6,5 @@ for (($i = 1000); $i -lt 1500; $i++) {
     $FileName = "$FilePrefix-$i.txt"
     Set-Content -Path $FileName -Value ("$($FillCharacter * $i)" + $Pattern + "$($FillCharacter * 10)")
 }
+
+# user:Liturgist repo:test003 content:THE_PATTERN path:PatTest*.txt
